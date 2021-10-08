f72a5d05ba3f3f9508940d5a6c3e0b2a3b194a38
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-10-08 12:29:05 +0200

    Create the class Predicates and update TestUtility.

--- src/test/java/it/units/sdm/gomoku/utils/TestUtility.java
+++ src/test/java/it/units/sdm/gomoku/utils/TestUtility.java
@@ -16,16 +16,17 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static it.units.sdm.gomoku.EnvVariables.CSV_NEW_LINE;
-import static it.units.sdm.gomoku.EnvVariables.CSV_SEPARATOR;
-import static it.units.sdm.gomoku.utils.TestUtility.*;
+import static it.units.sdm.gomoku.utils.IOUtility.CSV_NEW_LINE;
+import static it.units.sdm.gomoku.utils.IOUtility.CSV_SEPARATOR;
 import static it.units.sdm.gomoku.utils.Predicates.isNonEmptyString;
+import static it.units.sdm.gomoku.utils.TestUtility.createNxNRandomBoardToStringInCSVFormat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestUtility {
 
     @NotNull
-    static String createNxNRandomBoardToString(int N, int randomSeed) {
+    static String createNxNRandomBoardToStringInCSVFormat(int N, int randomSeed) {
         Random random = new Random(randomSeed);
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < N; i++) {
@@ -47,9 +48,9 @@ public class TestUtility {
     }
 
     @NotNull
-    public static String createNxNRandomBoardToString(int N) {
+    public static String createNxNRandomBoardToStringInCSVFormat(int N) {
         final int RANDOM_SEED = 0;
-        return createNxNRandomBoardToString(N, RANDOM_SEED);
+        return createNxNRandomBoardToStringInCSVFormat(N, RANDOM_SEED);
     }
 
     @NotNull
@@ -60,18 +61,46 @@ public class TestUtility {
                 .flatMap(i -> IntStream.range(0, N).mapToObj(j -> Arguments.of(i, j)));
     }
 
+    public static int getTotalNumberOfValidStoneInTheGivenBoarsAsStringInCSVFormat(@NotNull final String boardAsCSVString) {
+        Map<Board.Stone, Integer> countStonesPerType = getHowManyStonesPerTypeAsMapStartingFromStringRepresentingTheMatrixInCSVFormat(Objects.requireNonNull(boardAsCSVString));
+        return countStonesPerType.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    @NotNull
+    private static Map<Board.Stone, Integer> getHowManyStonesPerTypeAsMapStartingFromStringRepresentingTheMatrixInCSVFormat(@NotNull final String boardAsStringMatrix) {
+        return Arrays.stream(Board.Stone.values())
+                .unordered().parallel()
+                .map(stoneType -> new AbstractMap.SimpleEntry<>(
+                                stoneType,
+                                (int) getRowsAsStreamOfStringFromBoarsProvidedAsStringRepresentingTheMatrixInCSVFormat(Objects.requireNonNull(boardAsStringMatrix))
+                                        .flatMap(aCell -> aCell)
+                                        .map(Board.Stone::valueOf)
+                                        .filter(aCell -> aCell == stoneType)
+                                        .count()
+                        )
+                )
+                .collect(Collectors.toConcurrentMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @NotNull
+    static Stream<Stream<String>> getRowsAsStreamOfStringFromBoarsProvidedAsStringRepresentingTheMatrixInCSVFormat(@NotNull final String boardAsStringInCSVFormat) {
+        return Arrays.stream(Objects.requireNonNull(boardAsStringInCSVFormat).split(CSV_NEW_LINE))
+                .filter(isNonEmptyString)
+                .map(aLine -> Arrays.stream(aLine.split(CSV_SEPARATOR)));
+    }
+
 }
 
 class TestUtilityTest {
 
-    private final static int LIMIT_VALUE_FOR_N = 200; // avoid to create too big boards
+    private final static int LIMIT_VALUE_FOR_N = 200; // avoid creating too big boards
 
     @ParameterizedTest
     @CsvFileSource(resources = EnvVariables.INTS_PROVIDER_RESOURCE_LOCATION)
     void createNxNRandomBoardToStringTest_testMethodWithoutSeedParam(int N, TestInfo testInfo) {
         final int DEFAULT_RANDOM_SEED = 0;
         if (Math.abs(N) < LIMIT_VALUE_FOR_N) {
-            assertEquals(createNxNRandomBoardToString(N), createNxNRandomBoardToString(N, DEFAULT_RANDOM_SEED));
+            assertEquals(createNxNRandomBoardToStringInCSVFormat(N), createNxNRandomBoardToStringInCSVFormat(N, DEFAULT_RANDOM_SEED));
         } else {
             Optional<Method> thisMethod = testInfo.getTestMethod();
             Logger.getLogger(this.getClass().getCanonicalName())
@@ -83,37 +112,25 @@ class TestUtilityTest {
 
     @ParameterizedTest
     @MethodSource("provideCoupleOfNonNegativeIntegersTill19Excluded")
-    void createNxNRandomBoardToStringTest_assertHavingNxNStones(int N, int randomSeed) {
-        final int expectedNumberOfStonesToBePresent = N * N;
-        String boardAsString = createNxNRandomBoardToString(N, randomSeed);
-        Map<Board.Stone, Integer> countStonesPerType =
-                Arrays.stream(Board.Stone.values())
-                        .unordered().parallel()
-                        .map(stoneType -> new AbstractMap.SimpleEntry<>(
-                                        stoneType,
-                                        (int) Arrays.stream(boardAsString.split(CSV_NEW_LINE))
-                                                .flatMap(aLine -> Arrays.stream(aLine.split(CSV_SEPARATOR)))
-                                                .filter(isNonEmptyString)
-                                                .map(Board.Stone::valueOf)
-                                                .filter(aCell -> aCell == stoneType)
-                                                .count()
-                                )
-                        )
-                        .collect(Collectors.toConcurrentMap(Map.Entry::getKey, Map.Entry::getValue));
-        assertEquals(expectedNumberOfStonesToBePresent, countStonesPerType.values().stream().mapToInt(Integer::intValue).sum());
+    void createNxNRandomBoardToStringWithGivenRandomSeedTest_assertMatrixToBeSquaredOfSizeNxN(int N, int randomSeed) {
+        String boardAsCSVString = createNxNRandomBoardToStringInCSVFormat(N, randomSeed);
+        String[][] matrix = TestUtility.getRowsAsStreamOfStringFromBoarsProvidedAsStringRepresentingTheMatrixInCSVFormat(boardAsCSVString)
+                .map(aRow -> aRow.toArray(String[]::new))
+                .toArray(String[][]::new);
+        assertTrue(Predicates.isSquareMatrixOfGivenSize.test(matrix, N));
     }
 
     @ParameterizedTest
     @MethodSource("provideCoupleOfNonNegativeIntegersTill19Excluded")
-    void createNxNRandomBoardToStringTest_assertMatrixToBeSquaredOfSizeN(int N, int randomSeed) {
-        String boardAsString = createNxNRandomBoardToString(N, randomSeed);
-        List<String> lines = Arrays.stream(boardAsString.split(CSV_NEW_LINE))
-                .filter(isNonEmptyString)
-                .collect(Collectors.toList());
-        assertEquals(lines.size(), N);
+    void createNxNRandomBoardToStringTest_assertHavingNxNStones(int N, int randomSeed) {
+        final int expectedNumberOfStonesToBePresent = N * N;
+        String boardAsCSVString = createNxNRandomBoardToStringInCSVFormat(N, randomSeed);
+        int totalNumberOfValidStones = TestUtility.getTotalNumberOfValidStoneInTheGivenBoarsAsStringInCSVFormat(boardAsCSVString);
+        assertEquals(expectedNumberOfStonesToBePresent, totalNumberOfValidStones);
     }
 
-    private static Stream<Arguments> provideCoupleOfNonNegativeIntegersTill19Excluded() {
-        return provideCoupleOfNonNegativeIntegersTillNExcluded(19);
+    @NotNull
+    static Stream<Arguments> provideCoupleOfNonNegativeIntegersTill19Excluded() {
+        return TestUtility.provideCoupleOfNonNegativeIntegersTillNExcluded(19);
     }
 }
