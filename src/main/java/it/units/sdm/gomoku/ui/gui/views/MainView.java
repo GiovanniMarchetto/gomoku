2ede90f334227cdb847db7f64560b0578415ee51
Massimiliano Cristarella <m.cristarella@outlook.com>
2021-10-18 13:50:02 +0200

    Added Viewmodel to MainView

--- src/main/java/it/units/sdm/gomoku/ui/gui/views/MainView.java
+++ src/main/java/it/units/sdm/gomoku/ui/gui/views/MainView.java
@@ -1,10 +1,14 @@
 package it.units.sdm.gomoku.ui.gui.views;
 
+import it.units.sdm.gomoku.model.custom_types.Coordinates;
+import it.units.sdm.gomoku.model.entities.Board;
+import it.units.sdm.gomoku.ui.gui.viewmodels.MainViewmodel;
+import it.units.sdm.gomoku.ui.gui.viewmodels.Viewmodel;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
-public class MainView {
+public class MainView extends View {
 
     @FXML
     private AnchorPane mainAnchorPane;
@@ -15,15 +19,21 @@ public class MainView {
     @FXML
     private GridPane rightGridPane;
 
+    public MainView() {
+        super(new MainViewmodel());
+    }
+
     @FXML
     private void initialize() {
+
+        MainViewmodel vm = (MainViewmodel) getViewmodelAssociatedWithView();
 
         int boardSize = 19;
         double discardSafeMisure = 50;
         double discardHeight = topGridPane.getPrefHeight();// 50;
         double discardWidth = rightGridPane.getPrefWidth();// 100;
 
-        GomokuGridManager gomokuGridManager = new GomokuGridManager(boardSize, mainAnchorPane,
+        GomokuGridManager gomokuGridManager = new GomokuGridManager(vm, mainAnchorPane,
                 discardWidth + discardSafeMisure, discardHeight + discardSafeMisure);
         GridPane baseGridPane = gomokuGridManager.getGridPane();
 
