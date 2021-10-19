d2af1440b002405f872d4e719d51d1253eedd5f2
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-10-19 18:37:06 +0200

    Add ended version of summary view

--- src/main/java/it/units/sdm/gomoku/ui/gui/views/SummaryView.java
+++ src/main/java/it/units/sdm/gomoku/ui/gui/views/SummaryView.java
@@ -2,6 +2,7 @@ package it.units.sdm.gomoku.ui.gui.views;
 
 import it.units.sdm.gomoku.mvvm_library.views.View;
 import it.units.sdm.gomoku.mvvm_library.views.gui_items.CommanderButton;
+import it.units.sdm.gomoku.ui.gui.viewmodels.MainViewmodel;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
@@ -20,17 +21,20 @@ public class SummaryView extends View {
 
     @FXML
     private void initialize() {
-        CommanderButton continueButton = getButtonFirePropertyChange();
-        mainVBox.getChildren().add(continueButton.getGUIItem());
+        var vm = (MainViewmodel) getViewmodelAssociatedWithView();
+        if (!vm.isMatchEnded()) {
+            CommanderButton continueButton = getContinueButtonFirePropertyChange();
+            mainVBox.getChildren().add(continueButton.getGUIItem());
+        }
     }
 
-    private CommanderButton getButtonFirePropertyChange() {
+    private CommanderButton getContinueButtonFirePropertyChange() {
         return new CommanderButton(
                 "Continue",
                 this,
                 getViewmodelAssociatedWithView(),
                 continueAfterSummaryPropertyName,
-                ()->null);
+                () -> null);
     }
 
 }
