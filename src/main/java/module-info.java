10586cd7e4cc0242044c177ecf908d79a92f7159
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-10-21 11:24:57 +0200

    Reorder model-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,22 +6,22 @@ module it.units.sdm.gomoku {
     requires org.json;
     requires java.desktop;
 
+    opens it.units.sdm.gomoku to javafx.fxml;
+    opens it.units.sdm.gomoku.mvvm_library.views to javafx.fxml;
+    opens it.units.sdm.gomoku.ui.gui.views to javafx.fxml;
+    opens it.units.sdm.gomoku.ui.support to javafx.fxml;
+    opens it.units.sdm.gomoku.ui.gui to javafx.fxml;
 
     exports it.units.sdm.gomoku;
-    opens it.units.sdm.gomoku to javafx.fxml;
-    exports it.units.sdm.gomoku.ui.gui.views;
-    opens it.units.sdm.gomoku.ui.gui.views to javafx.fxml;
-    exports it.units.sdm.gomoku.ui.cli;
     exports it.units.sdm.gomoku.model.entities;
     exports it.units.sdm.gomoku.model.custom_types;
-    exports it.units.sdm.gomoku.ui.support;
-    opens it.units.sdm.gomoku.ui.support to javafx.fxml;
-    exports it.units.sdm.gomoku.ui.cli.io;
-    exports it.units.sdm.gomoku.mvvm_library.views;
-    opens it.units.sdm.gomoku.mvvm_library.views to javafx.fxml;
-    exports it.units.sdm.gomoku.mvvm_library.viewmodels;
-    exports it.units.sdm.gomoku.ui.gui;
-    opens it.units.sdm.gomoku.ui.gui to javafx.fxml;
-    exports it.units.sdm.gomoku.ui.gui.viewmodels;
     exports it.units.sdm.gomoku.mvvm_library;
+    exports it.units.sdm.gomoku.mvvm_library.views;
+    exports it.units.sdm.gomoku.mvvm_library.viewmodels;
+    exports it.units.sdm.gomoku.ui.cli;
+    exports it.units.sdm.gomoku.ui.cli.io;
+    exports it.units.sdm.gomoku.ui.gui;
+    exports it.units.sdm.gomoku.ui.gui.views;
+    exports it.units.sdm.gomoku.ui.gui.viewmodels;
+    exports it.units.sdm.gomoku.ui.support;
 }
