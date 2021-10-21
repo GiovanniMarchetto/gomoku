933572749766de3455ef6af8666024922a7aeaf2
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-10-21 11:16:59 +0200

    Remove utils folds from model-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,7 +12,6 @@ module it.units.sdm.gomoku {
     exports it.units.sdm.gomoku.ui.gui.views;
     opens it.units.sdm.gomoku.ui.gui.views to javafx.fxml;
     exports it.units.sdm.gomoku.ui.cli;
-    exports it.units.sdm.gomoku.model.utils;
     exports it.units.sdm.gomoku.model.entities;
     exports it.units.sdm.gomoku.model.custom_types;
     exports it.units.sdm.gomoku.ui.support;
