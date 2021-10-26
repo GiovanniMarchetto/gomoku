00d5a48e0cc14c8adf0509ecf994daf21519c732
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-10-26 12:43:14 +0200

    TDD: use parametrized test

--- src/test/java/it/units/sdm/gomoku/ui/cli/UtilityTest.java
+++ src/test/java/it/units/sdm/gomoku/ui/cli/UtilityTest.java
@@ -1,6 +1,8 @@
 package it.units.sdm.gomoku.ui.cli;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,11 +13,9 @@ class UtilityTest {
         assertTrue(Utility.isY('Y'));
     }
 
-    @Test
-    void isValidCharInserted() {
-        char insertedInput = 'Y';
-        char validInput = 'Y';
-        assertEquals(insertedInput==validInput,
-                Utility.isValidCharInserted(insertedInput,validInput));
+    @ParameterizedTest
+    @CsvSource({"true, a, a", "false, a, b", "false, a,C"})
+    void isValidCharInserted(boolean expected, char insertedInput, char validInput) {
+        assertEquals(expected, Utility.isValidCharInserted(insertedInput,validInput));
     }
 }
