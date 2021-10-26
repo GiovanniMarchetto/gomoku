d07ffbc9eefc4fc67b58e8bcd71125691e5d50e4
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-10-26 13:11:37 +0200

    Refactor: inline methods

--- src/main/java/it/units/sdm/gomoku/ui/cli/Utility.java
+++ src/main/java/it/units/sdm/gomoku/ui/cli/Utility.java
@@ -4,26 +4,11 @@ import java.util.Scanner;
 
 public class Utility {
 
-    public static boolean isValidCharInsertedCaseInsensitive(char insertedByTheUser, char validChar) {
-        return Character.toLowerCase(insertedByTheUser) == Character.toLowerCase(validChar);
-    }
-
-    public static boolean isYInsertedFromStdIn() {
-        try (Scanner userInput = new Scanner(System.in)) {
-            try {
-                char inserted = userInput.nextLine().toLowerCase().charAt(0);
-                return isValidCharInsertedCaseInsensitive(inserted, 'Y');
-            } catch (Exception invalidInputException) {
-                return false;
-            }
-        }
-    }
-
     public static boolean isValidCharInsertedFromStdIn(char validChar) {
         try (Scanner userInput = new Scanner(System.in)) {
             try {
                 char inserted = userInput.nextLine().toLowerCase().charAt(0);
-                return isValidCharInsertedCaseInsensitive(inserted, validChar);
+                return Character.toLowerCase(inserted) == Character.toLowerCase(validChar);
             } catch (Exception invalidInputException) {
                 return false;
             }
--- src/test/java/it/units/sdm/gomoku/ui/cli/UtilityTest.java
+++ src/test/java/it/units/sdm/gomoku/ui/cli/UtilityTest.java
@@ -1,6 +1,5 @@
 package it.units.sdm.gomoku.ui.cli;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,25 +7,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UtilityTest {
-
-    @ParameterizedTest
-    @CsvSource({"true, a, a", "false, a, b", "true, a,A"})
-    void isValidCharInsertedCaseInsensitive(boolean expected, char insertedInput, char validInput) {
-        assertEquals(expected, Utility.isValidCharInsertedCaseInsensitive(insertedInput, validInput));
-    }
-
-    @Test
-    void isYInsertedFromStdIn() {
-        try (ByteArrayInputStream fakeStdIn = new ByteArrayInputStream(new byte[]{'Y'})) {
-            System.setIn(fakeStdIn);
-            assertTrue(Utility.isYInsertedFromStdIn());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @ParameterizedTest
     @CsvSource({"true, a, a", "false, a, b", "true, a,A"})
