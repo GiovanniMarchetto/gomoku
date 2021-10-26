028ed7834c8a1b2c30364c62084c81977d349b5c
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-10-26 13:03:47 +0200

    TDD: add method to check if 'Y' is inserted into Standard Input

--- src/main/java/it/units/sdm/gomoku/ui/cli/Utility.java
+++ src/main/java/it/units/sdm/gomoku/ui/cli/Utility.java
@@ -1,9 +1,22 @@
 package it.units.sdm.gomoku.ui.cli;
 
+import java.util.Scanner;
+
 public class Utility {
 
     public static boolean isValidCharInsertedCaseInsensitive(char insertedByTheUser, char validChar) {
-        return Character.toLowerCase(insertedByTheUser)==Character.toLowerCase(validChar);
+        return Character.toLowerCase(insertedByTheUser) == Character.toLowerCase(validChar);
+    }
+
+    public static boolean isYInsertedFromStdIn() {
+        try (Scanner userInput = new Scanner(System.in)) {
+            try {
+                char inserted = userInput.nextLine().toLowerCase().charAt(0);
+                return isValidCharInsertedCaseInsensitive(inserted, 'Y');
+            } catch (Exception invalidInputException) {
+                return false;
+            }
+        }
     }
 
 }
