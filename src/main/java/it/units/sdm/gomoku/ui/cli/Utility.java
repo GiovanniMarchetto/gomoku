f4a8ac8ab55b642268eea69c1e81d379780e5092
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-10-26 12:34:48 +0200

    TDD: add method to check if a character is 'Y'

---
+++ src/main/java/it/units/sdm/gomoku/ui/cli/Utility.java
@@ -0,0 +1,9 @@
+package it.units.sdm.gomoku.ui.cli;
+
+public class Utility {
+
+    public static boolean isY(char insertedByTheUser) {
+        return insertedByTheUser=='Y';
+    }
+
+}
---
+++ src/test/java/it/units/sdm/gomoku/ui/cli/UtilityTest.java
@@ -0,0 +1,13 @@
+package it.units.sdm.gomoku.ui.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilityTest {
+
+    @Test
+    void isY() {
+        assertTrue(Utility.isY('Y'));
+    }
+}
