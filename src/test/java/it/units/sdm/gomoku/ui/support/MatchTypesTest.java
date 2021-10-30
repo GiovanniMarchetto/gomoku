862a5934a18e94a270edb7eefc005ef5a88698ae
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-10-30 09:26:25 +0200

    Refactoring MatchTypesTest

--- src/test/java/it/units/sdm/gomoku/ui/support/MatchTypesTest.java
+++ src/test/java/it/units/sdm/gomoku/ui/support/MatchTypesTest.java
@@ -6,39 +6,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MatchTypesTest {
 
+    private final MatchTypes cpuVsPerson = MatchTypes.CPU_VS_PERSON;
+    private final MatchTypes personVsPerson = MatchTypes.PERSON_VS_PERSON;
+
     @Test
     void getNumberOfHumanPlayersCPU_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.CPU_VS_PERSON;
-        assertEquals(1, matchTypes.getNumberOfHumanPlayers());
+        assertEquals(1, cpuVsPerson.getNumberOfHumanPlayers());
     }
 
     @Test
     void getNumberOfHumanPlayersPERSON_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.PERSON_VS_PERSON;
-        assertEquals(2, matchTypes.getNumberOfHumanPlayers());
+        assertEquals(2, personVsPerson.getNumberOfHumanPlayers());
     }
 
     @Test
     void testToStringCPU_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.CPU_VS_PERSON;
-        assertEquals("CPU VS PERSON", matchTypes.toString());
+        assertEquals("CPU VS PERSON", cpuVsPerson.toString());
     }
 
     @Test
     void testToStringPERSON_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.PERSON_VS_PERSON;
-        assertEquals("PERSON VS PERSON", matchTypes.toString());
+        assertEquals("PERSON VS PERSON", personVsPerson.toString());
     }
 
     @Test
     void getExposedValueOfCPU_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.CPU_VS_PERSON;
-        assertEquals("1", matchTypes.getExposedValueOf());
+        assertEquals("1", cpuVsPerson.getExposedValueOf());
     }
 
     @Test
     void getExposedValueOfPERSON_VS_PERSON() {
-        MatchTypes matchTypes = MatchTypes.PERSON_VS_PERSON;
-        assertEquals("2", matchTypes.getExposedValueOf());
+        assertEquals("2", personVsPerson.getExposedValueOf());
     }
 }
