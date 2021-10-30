96e698bf0d15cc82ff52e86ed64d00170639b284
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-10-30 12:43:05 +0200

    Create a Setup record

--- src/main/java/it/units/sdm/gomoku/ui/support/Setup.java
+++ src/main/java/it/units/sdm/gomoku/ui/support/Setup.java
@@ -1,66 +1,7 @@
 package it.units.sdm.gomoku.ui.support;
 
-import it.units.sdm.gomoku.Length;
 import it.units.sdm.gomoku.model.custom_types.PositiveInteger;
-import it.units.sdm.gomoku.model.custom_types.PositiveOddInteger;
 import it.units.sdm.gomoku.model.entities.Player;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Objects;
-
-public abstract class Setup {       // TODO : to be tested
-
-    @NotNull
-    protected final Player[] players;
-
-    @NotNull
-    protected final PositiveInteger numberOfGames;
-
-    @NotNull
-    protected final PositiveOddInteger boardSize;
-
-    protected Setup(@NotNull final Player[] players,
-                    @NotNull final PositiveInteger numberOfGames,
-                    @NotNull final PositiveOddInteger boardSizes) {
-        this.players = Objects.requireNonNull(players);
-        this.numberOfGames = Objects.requireNonNull(numberOfGames);
-        this.boardSize = Objects.requireNonNull(boardSizes);
-    }
-
-    protected Setup(@NotNull final Player playerOne,
-                    @NotNull final Player playerTwo,
-                    @NotNull final PositiveInteger numberOfGames,
-                    @NotNull final PositiveOddInteger boardSizes) {
-        this(
-                new Player[]{Objects.requireNonNull(playerOne), Objects.requireNonNull(playerTwo)},
-                numberOfGames, boardSizes
-        );
-    }
-
-    @NotNull
-    @Length(length = 2)
-    public Player[] getPlayers() {
-        return players;
-    }
-
-    @NotNull
-    public PositiveInteger getNumberOfGames() {
-        return numberOfGames;
-    }
-
-    @NotNull
-    public PositiveOddInteger getBoardSizeValue() {
-        return boardSize;
-    }
-
-    @Override
-    public String toString() {
-        return "Setup{" +
-                "players=" + Arrays.toString(players) +
-                ", numberOfGames=" + numberOfGames +
-                ", boardSize=" + boardSize +
-                '}';
-    }
-
+public record Setup(Player player1, Player player2, PositiveInteger numberOfGames, PositiveInteger boardSize) {
 }
