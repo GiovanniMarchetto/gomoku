6d4219dbd9f311506f0119c076a1fc41f90b060e
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-02 19:11:33 +0100

    Add fake server

---
+++ src/test/java/it/units/sdm/gomoku/client_server/fake_objects/ServerThatAcceptsOnlyOneRequestAndSavesItInField.java
@@ -0,0 +1,39 @@
+package it.units.sdm.gomoku.client_server.fake_objects;
+
+import it.units.sdm.gomoku.client_server.ClientServerUtility;
+import it.units.sdm.gomoku.client_server.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerThatAcceptsOnlyOneRequestAndSavesItInField implements Server {    // TODO : to be tested
+    private final ServerSocket serverSocket;
+    private volatile Socket acceptedClientSocket;
+
+    public ServerThatAcceptsOnlyOneRequestAndSavesItInField() throws IOException {
+        this.serverSocket = new ServerSocket(ClientServerUtility.SERVER_PORT_NUMBER);
+        this.acceptedClientSocket = null;
+    }
+
+    public Socket getAcceptedClientSocketOrNullIfNoClientsConnected() {
+        return acceptedClientSocket;
+    }
+
+    @Override
+    public void close() throws IOException {
+        serverSocket.close();
+    }
+
+    @Override
+    public void run() {
+        try {
+            acceptedClientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            Logger.getLogger(getClass().getCanonicalName())
+                    .log(Level.SEVERE, "I/O Exception", e);
+        }
+    }
+}
