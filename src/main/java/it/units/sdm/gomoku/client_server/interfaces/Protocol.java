428a637c82fbeb3512a06abd9d7add0a7732f4e6
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-03 17:44:33 +0100

    Now processInput() in Protocol throws IOException

--- src/main/java/it/units/sdm/gomoku/client_server/interfaces/Protocol.java
+++ src/main/java/it/units/sdm/gomoku/client_server/interfaces/Protocol.java
@@ -2,6 +2,8 @@ package it.units.sdm.gomoku.client_server.interfaces;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 public interface Protocol {
-    Object processInput(@NotNull final Object input);
+    Object processInput(@NotNull final Object input) throws IOException;
 }
