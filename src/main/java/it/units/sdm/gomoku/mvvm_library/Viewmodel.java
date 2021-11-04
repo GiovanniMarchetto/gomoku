6ad1f9886803b21447c81f237123058b5c2e1661
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-04 16:25:41 +0100

    Create runOnSeparateThread() in Viewmodel

--- src/main/java/it/units/sdm/gomoku/mvvm_library/Viewmodel.java
+++ src/main/java/it/units/sdm/gomoku/mvvm_library/Viewmodel.java
@@ -1,10 +1,16 @@
 package it.units.sdm.gomoku.mvvm_library;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.beans.PropertyChangeEvent;
 
 public abstract class Viewmodel implements
         Observer   /* Viewmodel observes the Model*/,
         Observable /* Viewmodel is observed by the View */ {
+
+    protected void runOnSeparateThread(@NotNull final Runnable runnable) {
+           new Thread(runnable).start();
+    }
 
     public abstract void propertyChange(PropertyChangeEvent evt);
 
