9548ca899a582191948d0848b8741529d5fac78f
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-05 16:26:46 +0100

    Move END_GAMES from EnvVariables to TestUtility

--- src/test/java/it/units/sdm/gomoku/EnvVariables.java
+++ src/test/java/it/units/sdm/gomoku/EnvVariables.java
@@ -10,7 +10,6 @@ public class EnvVariables {
     public final static String BOARD_19X19_PROVIDER_RESOURCE_LOCATION = "/boardExample19x19.csv";
     public final static String CSV_SAMPLE_FILE_2X2_INT_MATRIX_PROVIDER_RESOURCE_LOCATION = "/csvSampleFile2x2Ints.csv";
     public final static String CSV_SAMPLE_FILE_2X2_STRING_MATRIX_PROVIDER_RESOURCE_LOCATION = "/csvSampleFile2x2Strings.csv";
-    public final static String END_GAMES = "/endGames.json";
     public final static String COORDINATES_PROVIDER_RESOURCE_LOCATION = "/coordinatesFor19x19.csv";
 
     public static final PositiveInteger BOARD_SIZE = new PositiveInteger(19);
