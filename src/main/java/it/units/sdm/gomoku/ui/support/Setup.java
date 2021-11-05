1ead6e07f1a68ef083770c158663122d07157df5
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-05 15:25:57 +0100

    Reformat Setup

--- src/main/java/it/units/sdm/gomoku/ui/support/Setup.java
+++ src/main/java/it/units/sdm/gomoku/ui/support/Setup.java
@@ -3,5 +3,10 @@ package it.units.sdm.gomoku.ui.support;
 import it.units.sdm.gomoku.model.custom_types.PositiveInteger;
 import it.units.sdm.gomoku.model.entities.Player;
 
-public record Setup(Player player1, Player player2, PositiveInteger numberOfGames, PositiveInteger boardSize) {
+public record Setup(
+        Player player1,
+        Player player2,
+        PositiveInteger numberOfGames,
+        PositiveInteger boardSize
+) {
 }
