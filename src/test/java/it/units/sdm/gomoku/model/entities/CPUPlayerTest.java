f58831e9aa9802751ae2f8f3a64788c2326e3163
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-05 16:40:37 +0100

    Fix and refactoring CPUPlayerTest.

--- src/test/java/it/units/sdm/gomoku/model/entities/CPUPlayerTest.java
+++ src/test/java/it/units/sdm/gomoku/model/entities/CPUPlayerTest.java
@@ -10,39 +10,32 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static it.units.sdm.gomoku.model.entities.Board.NoMoreEmptyPositionAvailableException;
 import static it.units.sdm.gomoku.model.entities.Board.PositionAlreadyOccupiedException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CPUPlayerTest {
 
     public static final int NUMBER_OF_REPETITION = 10;
     private static final PositiveInteger BOARD_SIZE = new PositiveInteger(5);
-    private static Board boardBase = null;
     private static Board board = null;
     private final CPUPlayer cpuPlayer = new CPUPlayer("cpuPlayer");
     private Stone cpuStone = Stone.BLACK;
 
     @BeforeAll
-    static void setUp() {
-        boardBase = new Board(BOARD_SIZE);
+    static void resetBoard() {
+        board = new Board(BOARD_SIZE);
         try {
-            boardBase.occupyPosition(Stone.BLACK, new Coordinates(0, 1));
-            boardBase.occupyPosition(Stone.BLACK, new Coordinates(0, 3));
-            boardBase.occupyPosition(Stone.WHITE, new Coordinates(1, 2));
+            board.occupyPosition(Stone.BLACK, new Coordinates(0, 1));
+            board.occupyPosition(Stone.BLACK, new Coordinates(0, 3));
+            board.occupyPosition(Stone.WHITE, new Coordinates(1, 2));
         } catch (NoMoreEmptyPositionAvailableException | PositionAlreadyOccupiedException e) {
             e.printStackTrace();
         }
-        board = boardBase.clone();
-    }
-
-    private static void resetBoard() {
-        board = boardBase.clone();
     }
 
     @RepeatedTest(NUMBER_OF_REPETITION)
     void chooseRandomEmptyCoordinatesRepeatedTest() {
         try {
-            tryToOccupyCoordinatesChosen(cpuPlayer.chooseRandomEmptyCoordinates(board));
+            assertTrue(board.getStoneAtCoordinates(cpuPlayer.chooseRandomEmptyCoordinates(board)).isNone());
         } catch (NoMoreEmptyPositionAvailableException e) {
             if (board.isAnyEmptyPositionOnTheBoard()) {
                 fail(e);
@@ -56,6 +49,32 @@ class CPUPlayerTest {
             cpuStone = cpuStone == Stone.BLACK ? Stone.BLACK : Stone.WHITE;
         } catch (PositionAlreadyOccupiedException | NoMoreEmptyPositionAvailableException e) {
             fail(e);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2, 0,0", "4, 1,1", "5, 2,2", "9, 4,4"})
+    void chooseFromCenterFirstStone(int size, int x, int y) {
+        board = new Board(size);
+        Coordinates expected = new Coordinates(x, y);
+        try {
+            assertEquals(expected, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
+        } catch (NoMoreEmptyPositionAvailableException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2, 0,1", "4, 1,2", "5, 1,1", "9, 3,3"})
+    void chooseFromCenterSecondStone(int size, int x, int y) {
+        board = new Board(size);
+        try {
+            board.occupyPosition(Stone.BLACK, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
+
+            Coordinates expected = new Coordinates(x, y);
+            assertEquals(expected, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
+        } catch (NoMoreEmptyPositionAvailableException | PositionAlreadyOccupiedException e) {
+            fail(e.getMessage());
         }
     }
 
@@ -103,32 +122,6 @@ class CPUPlayerTest {
             }
         }
 
-        @ParameterizedTest
-        @CsvSource({"2, 0,0", "4, 1,1", "5, 2,2", "9, 4,4"})
-        void chooseFromCenterFirstStone(int size, int x, int y) {
-            board = new Board(size);
-            Coordinates expected = new Coordinates(x, y);
-            try {
-                assertEquals(expected, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
-            } catch (NoMoreEmptyPositionAvailableException e) {
-                fail(e.getMessage());
-            }
-        }
-
-        @ParameterizedTest
-        @CsvSource({"2, 0,1", "4, 1,2", "5, 1,1", "9, 3,3"})
-        void chooseFromCenterSecondStone(int size, int x, int y) {
-            board = new Board(size);
-            try {
-                board.occupyPosition(Stone.BLACK, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
-
-                Coordinates expected = new Coordinates(x, y);
-                assertEquals(expected, cpuPlayer.chooseNextEmptyCoordinatesFromCenter(board));
-            } catch (NoMoreEmptyPositionAvailableException | PositionAlreadyOccupiedException e) {
-                fail(e.getMessage());
-            }
-        }
     }
-
 
 }
