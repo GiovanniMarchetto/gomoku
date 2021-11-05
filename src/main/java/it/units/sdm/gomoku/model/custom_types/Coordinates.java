64f8650bbe153f6a8ddc23990629d04da8cce60e
Massimiliano Cristarella <m.cristarella@outlook.com>
2021-11-05 10:21:07 +0100

    Refactor Coordinates class

--- src/main/java/it/units/sdm/gomoku/model/custom_types/Coordinates.java
+++ src/main/java/it/units/sdm/gomoku/model/custom_types/Coordinates.java
@@ -1,6 +1,5 @@
 package it.units.sdm.gomoku.model.custom_types;
 
-import it.units.sdm.gomoku.Length;
 import javafx.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,31 +9,12 @@ import static it.units.sdm.gomoku.model.custom_types.NonNegativeInteger.NonNegat
 
 public class Coordinates extends Pair<NonNegativeInteger, NonNegativeInteger> {
 
-    /**
-     * Creates a new pair of coordinates
-     *
-     * @param x The x value
-     * @param y The y value
-     */
     public Coordinates(@NotNull NonNegativeInteger x, @NotNull NonNegativeInteger y) {
         super(Objects.requireNonNull(x), Objects.requireNonNull(y));
     }
 
-    public Coordinates(@NotNull @Length(length = 2) final NonNegativeInteger... coordinates) {
-        this(validateCoordsFromVarargs(coordinates)[0], coordinates[1]);
-    }
-
     public Coordinates(@NonNegativeIntegerType int x, @NonNegativeIntegerType int y) {
         this(new NonNegativeInteger(x), new NonNegativeInteger(y));
-    }
-
-    @NotNull
-    @Length(length = 2)
-    private static NonNegativeInteger[] validateCoordsFromVarargs(@NotNull final NonNegativeInteger[] coordinates) {
-        if (Objects.requireNonNull(coordinates).length != 2) {
-            throw new IllegalArgumentException("2 coordinates expected but " + coordinates.length + " found.");
-        }
-        return coordinates;
     }
 
     @NonNegativeIntegerType
@@ -52,13 +32,4 @@ public class Coordinates extends Pair<NonNegativeInteger, NonNegativeInteger> {
         return "(" + getX() + "," + getY() + ")";
     }
 
-    public static class InvalidCoordinateException extends Exception {
-        public InvalidCoordinateException() {
-            super();
-        }
-
-        public InvalidCoordinateException(@NotNull String msg) {
-            super(Objects.requireNonNull(msg));
-        }
-    }
 }
