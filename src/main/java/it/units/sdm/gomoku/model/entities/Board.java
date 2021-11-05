7be83de0d45fd391eaeef59cac425aede0c82872
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-05 20:09:55 +0100

    Reorder of Board methods

--- src/main/java/it/units/sdm/gomoku/model/entities/Board.java
+++ src/main/java/it/units/sdm/gomoku/model/entities/Board.java
@@ -28,10 +28,9 @@ public class Board implements Observable, Cloneable, Serializable {
     @NotNull
     private final Stone[][] matrix;
 
-
     public Board(@NotNull PositiveInteger size) {
         this.size = size;
-        coordinatesHistory = new ArrayList<>(size.intValue());
+        this.coordinatesHistory = new ArrayList<>(size.intValue());
         this.matrix = IntStream.range(0, size.intValue())
                 .unordered().parallel()
                 .mapToObj(a -> IntStream.range(0, size.intValue())
@@ -50,14 +49,32 @@ public class Board implements Observable, Cloneable, Serializable {
         this.matrix = board.getBoardMatrixCopy();
     }
 
-    public boolean isCoordinatesBelongingToChainOfNStones(@NotNull final Coordinates coords, NonNegativeInteger N) {
-        Stone stone = getStoneAtCoordinates(Objects.requireNonNull(coords));
-        if (stone.isNone()) {
-            throw new IllegalArgumentException("Given coordinates cannot refer to Stone." + stone);
-        }
-        return Stream.of(rowToList(coords), columnToList(coords), fwdDiagonalToList(coords), bckDiagonalToList(coords))
-                .unordered().parallel()
-                .anyMatch(stones -> Stone.isListContainingChainOfNStones(stones, N, stone));
+    @PositiveIntegerType
+    public int getSize() {
+        return size.intValue();
+    }
+
+    @NotNull
+    public List<Coordinates> getCoordinatesHistory() {
+        return coordinatesHistory;
+    }
+
+    public boolean isEmpty() {
+        return coordinatesHistory.size() == 0;
+    }
+
+    public synchronized boolean isAnyEmptyPositionOnTheBoard() {
+        return coordinatesHistory.size() < Math.pow(size.intValue(), 2);
+    }
+
+    private boolean isCoordinatesEmpty(@NotNull Coordinates coordinates) {
+        return getStoneAtCoordinates(Objects.requireNonNull(coordinates)).isNone();
+    }
+
+    public boolean isCoordinatesInsideBoard(@NotNull Coordinates coordinates) {
+        int x = Objects.requireNonNull(coordinates).getX();
+        int y = coordinates.getY();
+        return x < size.intValue() && y < size.intValue();
     }
 
     @NotNull
@@ -69,13 +86,10 @@ public class Board implements Observable, Cloneable, Serializable {
         }
     }
 
-    @NotNull
-    public List<Coordinates> getCoordinatesHistory() {
-        return coordinatesHistory;
-    }
-
-    public Stone[][] getBoardMatrix() {
-        return matrix;
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
+    @Override
+    public Board clone() {
+        return new Board(this);
     }
 
     public Stone[][] getBoardMatrixCopy() {
@@ -84,23 +98,14 @@ public class Board implements Observable, Cloneable, Serializable {
                 .toArray(Stone[][]::new);
     }
 
-    @PositiveIntegerType
-    public int getSize() {
-        return size.intValue();
-    }
-
-    public boolean isCoordinatesInsideBoard(@NotNull Coordinates coordinates) {
-        int x = Objects.requireNonNull(coordinates).getX();
-        int y = coordinates.getY();
-        return x < size.intValue() && y < size.intValue();
-    }
-
-    public boolean isEmpty() {
-        return coordinatesHistory.size() == 0;
-    }
-
-    public synchronized boolean isAnyEmptyPositionOnTheBoard() {
-        return coordinatesHistory.size() < Math.pow(size.intValue(), 2);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board otherBoard = (Board) o;
+        return size.equals(otherBoard.size)
+                && coordinatesHistory.size() == otherBoard.coordinatesHistory.size()
+                && Arrays.deepEquals(matrix, otherBoard.matrix);
     }
 
     public synchronized void occupyPosition(@NotNull Stone stone, @NotNull Coordinates coordinates)
@@ -118,10 +123,6 @@ public class Board implements Observable, Cloneable, Serializable {
         }
     }
 
-    private boolean isCoordinatesEmpty(@NotNull Coordinates coordinates) {
-        return getStoneAtCoordinates(Objects.requireNonNull(coordinates)).isNone();
-    }
-
     private void setStoneAtCoordinates(@NotNull final Coordinates coordinates, @NotNull Stone stone) {
         if (!stone.isNone()) {
             if (isCoordinatesInsideBoard(Objects.requireNonNull(coordinates))) {
@@ -134,14 +135,57 @@ public class Board implements Observable, Cloneable, Serializable {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Board otherBoard = (Board) o;
-        return size.equals(otherBoard.size)
-                && coordinatesHistory.size() == otherBoard.coordinatesHistory.size()
-                && Arrays.deepEquals(matrix, otherBoard.matrix);
+    public boolean isCoordinatesBelongingToChainOfNStones(@NotNull final Coordinates coords, NonNegativeInteger N) {
+        Stone stone = getStoneAtCoordinates(Objects.requireNonNull(coords));
+        if (stone.isNone()) {
+            throw new IllegalArgumentException("Given coordinates cannot refer to Stone." + stone);
+        }
+        return Stream.of(rowToList(coords), columnToList(coords), fwdDiagonalToList(coords), bckDiagonalToList(coords))
+                .unordered().parallel()
+                .anyMatch(stones -> Stone.isListContainingChainOfNStones(stones, N, stone));
+    }
+
+    @NotNull
+    private List<Stone> rowToList(@NotNull final Coordinates coords) {
+        return new ArrayList<>(Arrays.asList(matrix[Objects.requireNonNull(coords).getX()]));
+    }
+
+    @NotNull
+    private List<Stone> columnToList(@NotNull final Coordinates coords) {
+        Objects.requireNonNull(coords);
+        return IntStream
+                .range(0, getSize())
+                .sequential()
+                .mapToObj(x -> matrix[x][coords.getY()])
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    private List<Stone> fwdDiagonalToList(@NotNull final Coordinates coords) {
+        return diagonalToList(coords, false);
+    }
+
+    @NotNull
+    private List<Stone> bckDiagonalToList(@NotNull final Coordinates coords) {
+        return diagonalToList(coords, true);
+    }
+
+    @NotNull
+    private List<Stone> diagonalToList(@NotNull final Coordinates coords, boolean isBackDiagonal) {
+        int B = getSize();
+        int sign = isBackDiagonal ? -1 : 1;
+        int S = Objects.requireNonNull(coords).getX() + sign * coords.getY();
+
+        return IntStream.range(0, B).sequential()
+                .filter(i -> B + sign * i > sign * S && sign * i <= sign * S)
+                // sign =  1 => B + i >  S &&  i <=  S
+                // sign = -1 => B - i > -S && -i <= -S
+                .boxed()
+                .flatMap(i -> IntStream.range(0, B).unordered()
+                        .filter(j -> i + sign * j == S)
+                        .mapToObj(j -> new Coordinates(i, j)))
+                .map(this::getStoneAtCoordinates)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -183,61 +227,15 @@ public class Board implements Observable, Cloneable, Serializable {
                 .mapToObj(n -> " ").collect(Collectors.joining());
     }
 
-    @Override
-    public Board clone() {
-        return new Board(this);
-    }
-
-    @NotNull
-    private List<Stone> diagonalToList(@NotNull final Coordinates coords, boolean isBackDiagonal) {
-        int B = getSize();
-        int sign = isBackDiagonal ? -1 : 1;
-        int S = Objects.requireNonNull(coords).getX() + sign * coords.getY();
-
-        return IntStream.range(0, B).sequential()
-                .filter(i -> B + sign * i > sign * S && sign * i <= sign * S)
-                // sign =  1 => B + i >  S &&  i <=  S
-                // sign = -1 => B - i > -S && -i <= -S
-                .boxed()
-                .flatMap(i -> IntStream.range(0, B).unordered()
-                        .filter(j -> i + sign * j == S)
-                        .mapToObj(j -> new Coordinates(i, j)))
-                .map(this::getStoneAtCoordinates)
-                .collect(Collectors.toList());
-    }
-
-    @NotNull
-    private List<Stone> fwdDiagonalToList(@NotNull final Coordinates coords) {
-        return diagonalToList(coords, false);
-    }
-
-    @NotNull
-    private List<Stone> bckDiagonalToList(@NotNull final Coordinates coords) {
-        return diagonalToList(coords, true);
-    }
-
-    @NotNull
-    private List<Stone> columnToList(@NotNull final Coordinates coords) {
-        Objects.requireNonNull(coords);
-        return IntStream
-                .range(0, getSize())
-                .sequential()
-                .mapToObj(x -> matrix[x][coords.getY()])
-                .collect(Collectors.toList());
-    }
-
-    @NotNull
-    private List<Stone> rowToList(@NotNull final Coordinates coords) {
-        return new ArrayList<>(Arrays.asList(matrix[Objects.requireNonNull(coords).getX()]));
-    }
-
     public static class NoMoreEmptyPositionAvailableException extends Exception {
+
         public NoMoreEmptyPositionAvailableException() {
             super("The board is entirely filled. No more space available.");
         }
     }
 
     public static class PositionAlreadyOccupiedException extends Exception {
+
         public PositionAlreadyOccupiedException(@NotNull final Coordinates coordinates) {
             super(Objects.requireNonNull(coordinates) + " already occupied.");
         }
