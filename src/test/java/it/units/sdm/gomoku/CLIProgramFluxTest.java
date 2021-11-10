3a546f21f98db4c1bc9bbaafeeead9ba1fae802f
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-10 17:23:41 +0100

    Add test to check the first view created after launching the CLI application

--- src/test/java/it/units/sdm/gomoku/CLIProgramFluxTest.java
+++ src/test/java/it/units/sdm/gomoku/CLIProgramFluxTest.java
@@ -1,18 +1,34 @@
 package it.units.sdm.gomoku;
 
+import it.units.sdm.gomoku.mvvm_library.View;
 import it.units.sdm.gomoku.ui.cli.CLIMain;
 import it.units.sdm.gomoku.ui.cli.CLISceneController;
+import it.units.sdm.gomoku.ui.cli.views.CLIStartView;
+import it.units.sdm.gomoku.utils.TestUtility;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CLIProgramFluxTest {
+
+    private static final Supplier<CLISceneController> cliSceneControllerInstanceGetter = () -> {
+        try {
+            Method cliSceneControllerInstanceGetterMethod = CLISceneController.class.getDeclaredMethod("getInstance");
+            cliSceneControllerInstanceGetterMethod.setAccessible(true);
+            return (CLISceneController) cliSceneControllerInstanceGetterMethod.invoke(null);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            fail(e);
+            return null;
+        }
+    };
 
     @BeforeAll
     static void ignoreStdOut() {
@@ -20,7 +36,7 @@ public class CLIProgramFluxTest {
     }
 
     @Test
-    void sceneControllerInstantiatedAfterLaunchingTheApplication() {
+    void launchApplicationAndCheckSceneControllerInstantiation() {
         try {
             Method launcher = CLIMain.class.getDeclaredMethod("launch");
             launcher.setAccessible(true);
@@ -32,6 +48,18 @@ public class CLIProgramFluxTest {
             }
             assertTrue((boolean) wasSceneControllerAlreadyInstantiatedMethod.invoke(null));
         } catch (Exception e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void checkFirstView() {
+        launchApplicationAndCheckSceneControllerInstantiation();
+        View<?> actualCurrentView = null;
+        try {
+            actualCurrentView = (View<?>) TestUtility.getFieldValue("currentView", cliSceneControllerInstanceGetter.get());
+            assertTrue(actualCurrentView instanceof CLIStartView);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             fail(e);
         }
     }
