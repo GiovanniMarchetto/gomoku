5c3e51e552bf03ba3f060e2992cc654493248b29
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-11 13:34:03 +0100

    Add list containing all the observed properties in class View

--- src/main/java/it/units/sdm/gomoku/mvvm_library/View.java
+++ src/main/java/it/units/sdm/gomoku/mvvm_library/View.java
@@ -1,18 +1,38 @@
 package it.units.sdm.gomoku.mvvm_library;
 
+import it.units.sdm.gomoku.property_change_handlers.ObservableProperty;
+import it.units.sdm.gomoku.property_change_handlers.PropertyObserver;
+import org.jetbrains.annotations.NotNull;
+
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 public abstract class View<T extends Viewmodel> implements Observable {  // TODO : should be observer? (some views implement Observer)
 
+    @NotNull
     private final T viewmodelAssociatedWithView;
+    @NotNull
+    private final List<PropertyObserver<?>> propertiesObservedInViewModel;
 
-    public View(T viewmodelAssociatedWithView) {
-        this.viewmodelAssociatedWithView = viewmodelAssociatedWithView;
-        this.viewmodelAssociatedWithView.observe(this);     // bind viewModel -> view
+    public View(@NotNull T viewmodelAssociatedWithView) {
+        this.viewmodelAssociatedWithView = Objects.requireNonNull(viewmodelAssociatedWithView);
+        this.viewmodelAssociatedWithView.observe(this);     // bind viewModel -> view   // TODO : needed?
+        this.propertiesObservedInViewModel = new ArrayList<>();
     }
 
-    public T getViewmodelAssociatedWithView() {
+    public @NotNull T getViewmodelAssociatedWithView() {
         return viewmodelAssociatedWithView;
     }
 
     public void onViewInitialized() {
+    }
+
+    protected <S> void addObservedPropertyOfViewmodel(@NotNull final ObservableProperty<S> observedProperty,
+                                                      @NotNull final Consumer<PropertyChangeEvent> actionOnPropertyChange) {
+        propertiesObservedInViewModel.add(new PropertyObserver<>(
+                Objects.requireNonNull(observedProperty), Objects.requireNonNull(actionOnPropertyChange)));
     }
 }
