99f144c44efc8f32e510a7672a06bfe6d28c031d
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-13 12:55:57 +0100

    PlayerTest: redo

--- src/test/java/it/units/sdm/gomoku/model/entities/player/PlayerTest.java
+++ src/test/java/it/units/sdm/gomoku/model/entities/player/PlayerTest.java
@@ -1,19 +1,69 @@
 package it.units.sdm.gomoku.model.entities.player;
 
-import it.units.sdm.gomoku.EnvVariables;
 import it.units.sdm.gomoku.model.entities.HumanPlayer;
 import it.units.sdm.gomoku.model.entities.Player;
+import it.units.sdm.gomoku.property_change_handlers.ObservableProperty;
+import it.units.sdm.gomoku.utils.TestUtility;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    String name = "player";
+    Player player;
+
+    @BeforeEach
+    void setup() {
+        player = new HumanPlayer(name);
+    }
+
+    @Test
+    void getName() {
+        assertEquals(name, player.getName());
+    }
 
     @ParameterizedTest
-    @CsvFileSource(resources = EnvVariables.PLAYERS_NAME_PROVIDER_RESOURCE_LOCATION)
+    @ValueSource(strings = {"massimiliano", "matteo", "giovanni", "travis", "scott"})
     void testToString(String name) {
         Player player = new HumanPlayer(name);
         assertEquals(player.toString(), name);
+    }
+
+    @Test
+    void getCoordinatesRequiredToContinueProperty() {
+        try {
+            @SuppressWarnings("unchecked")
+            ObservableProperty<Boolean> coordinatesRequiredToContinueProperty = (ObservableProperty<Boolean>)
+                    TestUtility.getFieldValue("coordinatesRequiredToContinueProperty", player);
+            assertEquals(coordinatesRequiredToContinueProperty, player.getCoordinatesRequiredToContinueProperty());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void checkCoordinatesRequiredAtStart() {
+        assertEquals(Boolean.FALSE, player.getCoordinatesRequiredToContinueProperty().getPropertyValue());
+    }
+
+    @Test
+    void testEquals() {
+        Player player2 = new HumanPlayer(name);
+        assertEquals(player, player2);
+    }
+
+    @Test
+    void testEquality() {
+        Player player1 = new HumanPlayer("player");
+        Player player2 = new HumanPlayer("player");
+        assertNotSame(player1, player2);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(name.hashCode(), player.hashCode());
     }
 }
