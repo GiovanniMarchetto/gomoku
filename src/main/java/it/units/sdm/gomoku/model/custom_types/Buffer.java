838a6e32e074000c56c47a3214671a526298dc4c
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-14 18:45:00 +0100

    Add method insert in class Buffer and test

--- src/main/java/it/units/sdm/gomoku/model/custom_types/Buffer.java
+++ src/main/java/it/units/sdm/gomoku/model/custom_types/Buffer.java
@@ -1,9 +1,12 @@
 package it.units.sdm.gomoku.model.custom_types;
 
+import it.units.sdm.gomoku.Utility;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 import static it.units.sdm.gomoku.model.custom_types.NonNegativeInteger.NonNegativeIntegerType;
 import static it.units.sdm.gomoku.model.custom_types.PositiveInteger.PositiveIntegerType;
@@ -26,6 +29,18 @@ public class Buffer<ContentType> {
     @NonNegativeIntegerType
     public synchronized int getNumberOfElements() {
         return buffer.size();
+    }
+
+    public synchronized void insert(@Nullable final ContentType element) {
+        while (getNumberOfElements() == size) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Utility.getLoggerOfClass(getClass()).log(Level.WARNING, "Interrupted", e);
+            }
+        }
+        buffer.add(element);
+        notify();
     }
 
 }
--- src/test/java/it/units/sdm/gomoku/model/custom_types/BufferTest.java
+++ src/test/java/it/units/sdm/gomoku/model/custom_types/BufferTest.java
@@ -3,12 +3,14 @@ package it.units.sdm.gomoku.model.custom_types;
 import it.units.sdm.gomoku.EnvVariables;
 import it.units.sdm.gomoku.Utility;
 import it.units.sdm.gomoku.utils.TestUtility;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -56,6 +58,14 @@ class BufferTest {
         }
     }
 
+    private static void fillBufferWithIntegers(@NotNull final Buffer<Integer> buffer)
+            throws NoSuchFieldException, IllegalAccessException {
+        //noinspection ConstantConditions
+        IntStream.range(0, (int) TestUtility.getFieldValue("size", buffer))
+                .boxed()
+                .forEach(buffer::insert);
+    }
+
     @ParameterizedTest
     @CsvFileSource(resources = EnvVariables.POSITIVE_INTS_PROVIDER_RESOURCE_LOCATION)
     void getNumberOfElementsTest(int size) throws NoSuchFieldException, IllegalAccessException {
@@ -64,9 +74,44 @@ class BufferTest {
             @SuppressWarnings("unchecked")  // Buffer instance hosts elements of that type
             List<Integer> actualBuffer = (List<Integer>) TestUtility.getFieldValue("buffer", bufferInstance);
             assert actualBuffer != null;
-            actualBuffer.addAll(IntStream.range(0, numberOfInsertedElements).boxed().collect(Collectors.toList()));
+            actualBuffer.addAll(IntStream.range(0, numberOfInsertedElements).boxed().toList());
             assertEquals(numberOfInsertedElements, bufferInstance.getNumberOfElements());
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("it.units.sdm.gomoku.model.custom_types.CoordinatesTest#nonNegativeIntegerPairAndValidFlagSupplier"/* whatever supplier of parameters is ok */)
+    <ElementType> void insertOneElementWhenBufferIsEmpty(ElementType element) {
+        final int ARBITRARY_CHOSEN_SIZE = 10;
+        Buffer<ElementType> buffer = new Buffer<>(ARBITRARY_CHOSEN_SIZE);
+        assert isBufferEmptyImmediatelyAfterCreation(buffer);
+        buffer.insert(element);
+        final int NUMBER_OF_INSERTED_ELEMENTS = 1;
+        assertEquals(NUMBER_OF_INSERTED_ELEMENTS, buffer.getNumberOfElements());
+    }
+
+    private <ElementType> boolean isBufferEmptyImmediatelyAfterCreation(@NotNull final Buffer<ElementType> buffer) {
+        return Objects.requireNonNull(buffer).getNumberOfElements() == 0;
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = EnvVariables.POSITIVE_INTS_PROVIDER_RESOURCE_LOCATION)
+    void insertShouldWaitIfBufferIsFull() throws NoSuchFieldException, IllegalAccessException {
+        final int ARBITRARY_CHOSEN_SIZE = 10;
+        Buffer<Integer> buffer = new Buffer<>(ARBITRARY_CHOSEN_SIZE);
+        fillBufferWithIntegers(buffer);
+        assert buffer.getNumberOfElements() == ARBITRARY_CHOSEN_SIZE;
+        final Integer whateverIntegerElement = 1;
+        Thread threadThatTryToInsertOneMoreElementInBuffer = new Thread(() -> buffer.insert(whateverIntegerElement));
+        threadThatTryToInsertOneMoreElementInBuffer.setName("threadThatTryToInsertOneMoreElementInBuffer");
+        threadThatTryToInsertOneMoreElementInBuffer.start();
+        try {
+            final int REASONABLE_MILLISECS_TO_PERMIT_THE_THREAD_TO_START_AND_TRY_TO_INVOKE_INSERT_METHODS_OF_BUFFER = 1;
+            Thread.sleep(REASONABLE_MILLISECS_TO_PERMIT_THE_THREAD_TO_START_AND_TRY_TO_INVOKE_INSERT_METHODS_OF_BUFFER);
+        } catch (InterruptedException e) {
+            fail(e);
+        }
+        assertEquals(Thread.State.WAITING, threadThatTryToInsertOneMoreElementInBuffer.getState());
     }
 
 }
