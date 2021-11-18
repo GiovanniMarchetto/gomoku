21051ba503d34c06fbf5a42a22cde25f2a495dd4
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-18 14:08:35 +0100

    Move inner class ObservablePropertyTest.FakeObservableProperty to upper level and rename into ConcreteFakeObservableProperty

---
+++ src/test/java/it/units/sdm/gomoku/property_change_handlers/ConcreteFakeObservableProperty.java
@@ -0,0 +1,131 @@
+package it.units.sdm.gomoku.property_change_handlers;
+
+import it.units.sdm.gomoku.property_change_handlers.observable_properties.ObservableProperty;
+import it.units.sdm.gomoku.utils.TestUtility;
+import javafx.util.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ConcreteFakeObservableProperty<T> extends ObservableProperty<T> {
+    private static final String propertyNameFieldName = "propertyName";
+    private static final String propertyValueContainerFieldName = "propertyValueContainer";
+    private static final String propertyValueFieldName = "value";
+    private static final String numberOfDistinctCreatedInstancesFieldName = "numberOfDistinctCreatedInstances";
+
+    public ConcreteFakeObservableProperty() {
+        super();
+    }
+
+    public ConcreteFakeObservableProperty(ObservableProperty<T> observableProperty) {
+        super(observableProperty);
+    }
+
+    public static int getTotalNumberOfExistingDistinctInstances() {
+        try {
+            return (int) TestUtility
+                    .getFieldAlreadyMadeAccessible(ObservableProperty.class, numberOfDistinctCreatedInstancesFieldName)
+                    .get(null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+            return -1;
+        }
+    }
+
+    @NotNull
+    public static Pair<ConcreteFakeObservableProperty<String>, ConcreteFakeObservableProperty<String>>
+    createTwoInstancesWithEitherSameNameOrSameValueAndGet(boolean sameNameAndDifferentValue) {
+        String sampleString = "a";
+        String differentSampleString = sampleString + "b";
+        ConcreteFakeObservableProperty<String> one = ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(sampleString, sampleString);
+        ConcreteFakeObservableProperty<String> other;
+        if (sameNameAndDifferentValue) {
+            other = ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(sampleString, differentSampleString);
+        } else {
+            other = ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(differentSampleString, sampleString);
+        }
+        return new Pair<>(one, other);
+    }
+
+    @NotNull
+    public static <T> ConcreteFakeObservableProperty<T> createWithNameAndValueAlreadySet(@NotNull final String propertyName, @Nullable final T propertyValue) {
+        ConcreteFakeObservableProperty<T> theInstance = new ConcreteFakeObservableProperty<>();
+        theInstance.setPropertyNameUsingReflection(propertyName);
+        theInstance.setPropertyValueUsingReflection(propertyValue);
+        return theInstance;
+    }
+
+    @NotNull
+    public static ConcreteFakeObservableProperty<String> createInstanceAndGetItsCopy(String propertyName, String propertyValue) {
+        return new ConcreteFakeObservableProperty<>(
+                ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(propertyName, propertyValue));
+    }
+
+    @Override
+    @NotNull
+    public String getPropertyName() {
+        return super.getPropertyName();
+    }
+
+    @NotNull
+    public String getPropertyNameUsingReflection() {
+        try {
+            return (String) Objects.requireNonNull(TestUtility.getFieldValue(propertyNameFieldName, this));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+            return null;
+        }
+    }
+
+    public void setPropertyNameUsingReflection(@NotNull final String propertyName) {
+        try {
+            TestUtility.setFieldValue(propertyNameFieldName, Objects.requireNonNull(propertyName), this);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+        }
+    }
+
+    @Nullable
+    public Object getPropertyValueUsingReflection() {
+        try {
+            return TestUtility.getFieldValue(propertyValueFieldName, getPropertyValueContainer());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+            return null;
+        }
+    }
+
+    public void setPropertyValueUsingReflection(@Nullable final T propertyValue) {
+        try {
+            TestUtility.setFieldValue(propertyValueFieldName, propertyValue, getPropertyValueContainer());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+        }
+    }
+
+    @Override
+    @NotNull
+    protected ObservableProperty<T> setPropertyValueAndFireIfPropertyChange(@Nullable T newPropertyValue) {
+        return super.setPropertyValueAndFireIfPropertyChange(newPropertyValue);
+    }
+
+    @Override
+    @NotNull
+    protected synchronized ObservableProperty<T> setPropertyValueWithoutNotifying(@Nullable T newPropertyValue) {
+        return super.setPropertyValueWithoutNotifying(newPropertyValue);
+    }
+
+    @Override
+    public String toString() {
+        return "ConcreteFakeObservableProperty{propertyName=" + getPropertyNameUsingReflection() +
+                ", propertyValue=" + getPropertyValueUsingReflection() + "}";
+    }
+
+    @NotNull
+    private Object getPropertyValueContainer() throws NoSuchFieldException, IllegalAccessException {
+        return Objects.requireNonNull(TestUtility.getFieldValue(propertyValueContainerFieldName, this));
+    }
+}
--- src/test/java/it/units/sdm/gomoku/property_change_handlers/ObservablePropertyTest.java
+++ src/test/java/it/units/sdm/gomoku/property_change_handlers/ObservablePropertyTest.java
@@ -1,10 +1,6 @@
 package it.units.sdm.gomoku.property_change_handlers;
 
-import it.units.sdm.gomoku.property_change_handlers.observable_properties.ObservableProperty;
-import it.units.sdm.gomoku.utils.TestUtility;
 import javafx.util.Pair;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,7 +8,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.stream.IntStream;
@@ -23,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ObservablePropertyTest {
 
     private static final String sampleString = "init";
-    private FakeObservableProperty<String> observable;
+    private ConcreteFakeObservableProperty<String> observable;
 
     public static Stream<Arguments> allCombinationOf4RandomDistinctStrings() {
         int numberOfDistinct = 4;
@@ -40,34 +35,34 @@ class ObservablePropertyTest {
 
     @BeforeEach
     void setup() {
-        observable = new FakeObservableProperty<>();
+        observable = new ConcreteFakeObservableProperty<>();
     }
 
     @Test
     void createNewInstanceWithNullValue() {
-        assertNull(new FakeObservableProperty<>().getPropertyValue());
+        assertNull(new ConcreteFakeObservableProperty<>().getPropertyValue());
     }
 
     @ParameterizedTest
     @CsvSource({"1243, sample"})
     void createCopyInstanceWithSameName(String propertyName, String propertyValue) {
-        FakeObservableProperty<String> copiedObject =
-                FakeObservableProperty.createInstanceAndGetItsCopy(propertyName, propertyValue);
+        ConcreteFakeObservableProperty<String> copiedObject =
+                ConcreteFakeObservableProperty.createInstanceAndGetItsCopy(propertyName, propertyValue);
         assertEquals(propertyName, copiedObject.getPropertyNameUsingReflection());
     }
 
     @ParameterizedTest
     @CsvSource({"1243, sample"})
     void createCopyInstanceWithSameValue(String propertyName, String propertyValue) {
-        FakeObservableProperty<String> copiedObject =
-                FakeObservableProperty.createInstanceAndGetItsCopy(propertyName, propertyValue);
+        ConcreteFakeObservableProperty<String> copiedObject =
+                ConcreteFakeObservableProperty.createInstanceAndGetItsCopy(propertyName, propertyValue);
         assertEquals(propertyValue, copiedObject.getPropertyValueUsingReflection());
     }
 
     @Test
     void getPropertyName() {
         String expectedName = String.valueOf(
-                FakeObservableProperty.getTotalNumberOfExistingDistinctInstances() - 1);
+                ConcreteFakeObservableProperty.getTotalNumberOfExistingDistinctInstances() - 1);
         String nameReturnedFromGetter = observable.getPropertyName();
         assertEquals(expectedName, nameReturnedFromGetter);
     }
@@ -119,23 +114,23 @@ class ObservablePropertyTest {
 
     @Test
     void notEqualIfDifferentPropertyNames() {
-        Pair<FakeObservableProperty<String>, FakeObservableProperty<String>> pair =
-                FakeObservableProperty.createTwoInstancesWithEitherSameNameOrSameValueAndGet(true);
+        Pair<ConcreteFakeObservableProperty<String>, ConcreteFakeObservableProperty<String>> pair =
+                ConcreteFakeObservableProperty.createTwoInstancesWithEitherSameNameOrSameValueAndGet(true);
         assertNotEquals(pair.getKey(), pair.getValue());
     }
 
     @Test
     void notEqualIfDifferentPropertyValues() {
-        Pair<FakeObservableProperty<String>, FakeObservableProperty<String>> pair =
-                FakeObservableProperty.createTwoInstancesWithEitherSameNameOrSameValueAndGet(false);
+        Pair<ConcreteFakeObservableProperty<String>, ConcreteFakeObservableProperty<String>> pair =
+                ConcreteFakeObservableProperty.createTwoInstancesWithEitherSameNameOrSameValueAndGet(false);
         assertNotEquals(pair.getKey(), pair.getValue());
     }
 
     @ParameterizedTest
     @MethodSource("allCombinationOf4RandomDistinctStrings")
     void equalIfSameNameAndValue(String name1, String name2, String value1, String value2) {
-        FakeObservableProperty<String> one = FakeObservableProperty.createWithNameAndValueAlreadySet(name1, value1);
-        FakeObservableProperty<String> other = FakeObservableProperty.createWithNameAndValueAlreadySet(name2, value2);
+        ConcreteFakeObservableProperty<String> one = ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(name1, value1);
+        ConcreteFakeObservableProperty<String> other = ConcreteFakeObservableProperty.createWithNameAndValueAlreadySet(name2, value2);
         boolean expectedEqual = name1.equals(name2) && value1.equals(value2);
         assertEquals(expectedEqual, one.equals(other));
     }
@@ -143,122 +138,4 @@ class ObservablePropertyTest {
     // TODO: test hashcode + hashCode-equals contract
 
 
-    private static class FakeObservableProperty<T> extends ObservableProperty<T> {
-        private static final String propertyNameFieldName = "propertyName";
-        private static final String propertyValueContainerFieldName = "propertyValueContainer";
-        private static final String propertyValueFieldName = "value";
-        private static final String numberOfDistinctCreatedInstancesFieldName = "numberOfDistinctCreatedInstances";
-
-        public FakeObservableProperty() {
-            super();
-        }
-
-        public FakeObservableProperty(ObservableProperty<T> observableProperty) {
-            super(observableProperty);
-        }
-
-        public static int getTotalNumberOfExistingDistinctInstances() {
-            try {
-                return (int) TestUtility
-                        .getFieldAlreadyMadeAccessible(ObservableProperty.class, numberOfDistinctCreatedInstancesFieldName)
-                        .get(null);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                fail(e);
-                return -1;
-            }
-        }
-
-        @NotNull
-        public static Pair<FakeObservableProperty<String>, FakeObservableProperty<String>>
-        createTwoInstancesWithEitherSameNameOrSameValueAndGet(boolean sameNameAndDifferentValue) {
-            String differentSampleString = sampleString + "a";
-            FakeObservableProperty<String> one = FakeObservableProperty.createWithNameAndValueAlreadySet(sampleString, sampleString);
-            FakeObservableProperty<String> other;
-            if (sameNameAndDifferentValue) {
-                other = FakeObservableProperty.createWithNameAndValueAlreadySet(sampleString, differentSampleString);
-            } else {
-                other = FakeObservableProperty.createWithNameAndValueAlreadySet(differentSampleString, sampleString);
-            }
-            return new Pair<>(one, other);
-        }
-
-        @NotNull
-        public static <T> FakeObservableProperty<T> createWithNameAndValueAlreadySet(@NotNull final String propertyName, @Nullable final T propertyValue) {
-            FakeObservableProperty<T> theInstance = new FakeObservableProperty<>();
-            theInstance.setPropertyNameUsingReflection(propertyName);
-            theInstance.setPropertyValueUsingReflection(propertyValue);
-            return theInstance;
-        }
-
-        @NotNull
-        public static FakeObservableProperty<String> createInstanceAndGetItsCopy(String propertyName, String propertyValue) {
-            return new FakeObservableProperty<>(
-                    FakeObservableProperty.createWithNameAndValueAlreadySet(propertyName, propertyValue));
-        }
-
-        @Override
-        @NotNull
-        public String getPropertyName() {
-            return super.getPropertyName();
-        }
-
-        @NotNull
-        public String getPropertyNameUsingReflection() {
-            try {
-                return (String) TestUtility.getFieldValue(propertyNameFieldName, this);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                fail(e);
-                return null;
-            }
-        }
-
-        public void setPropertyNameUsingReflection(@NotNull final String propertyName) {
-            try {
-                TestUtility.setFieldValue(propertyNameFieldName, Objects.requireNonNull(propertyName), this);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                fail(e);
-            }
-        }
-
-        @Nullable
-        public Object getPropertyValueUsingReflection() {
-            try {
-                return TestUtility.getFieldValue(propertyValueFieldName, getPropertyValueContainer());
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                fail(e);
-                return null;
-            }
-        }
-
-        public void setPropertyValueUsingReflection(@Nullable final T propertyValue) {
-            try {
-                TestUtility.setFieldValue(propertyValueFieldName, propertyValue, getPropertyValueContainer());
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                fail(e);
-            }
-        }
-
-        @Override
-        @NotNull
-        protected ObservableProperty<T> setPropertyValueAndFireIfPropertyChange(@Nullable T newPropertyValue) {
-            return super.setPropertyValueAndFireIfPropertyChange(newPropertyValue);
-        }
-
-        @Override
-        @NotNull
-        protected synchronized ObservableProperty<T> setPropertyValueWithoutNotifying(@Nullable T newPropertyValue) {
-            return super.setPropertyValueWithoutNotifying(newPropertyValue);
-        }
-
-        @Override
-        public String toString() {
-            return "FakeObservableProperty{propertyName=" + getPropertyNameUsingReflection() +
-                    ", propertyValue=" + getPropertyValueUsingReflection() + "}";
-        }
-
-        @NotNull
-        private Object getPropertyValueContainer() throws NoSuchFieldException, IllegalAccessException {
-            return Objects.requireNonNull(TestUtility.getFieldValue(propertyValueContainerFieldName, this));
-        }
-    }
 }
