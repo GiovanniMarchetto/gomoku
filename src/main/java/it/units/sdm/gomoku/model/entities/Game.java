4cff2108ba10a3cd2de2144a99cc7becb47bb7ea
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-11-23 00:42:23 +0100

    Remove TODOS

--- src/main/java/it/units/sdm/gomoku/model/entities/Game.java
+++ src/main/java/it/units/sdm/gomoku/model/entities/Game.java
@@ -245,8 +245,5 @@ public class Game implements Comparable<Game>, Observable {
         return board.getStreamOfEmptyCoordinates();
     }
 
-    // TODO: equals and hashcode?
-
     public enum Status {NOT_STARTED, STARTED, ENDED}
-
 }
