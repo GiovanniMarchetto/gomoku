72f29fb884d8c5b31b71e386116d2d10f47a6442
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-22 19:51:15 +0100

    Add method Utility.isEvenNumber and test

---
+++ src/test/java/it/units/sdm/gomoku/utils/UtilityTest.java
@@ -0,0 +1,15 @@
+package it.units.sdm.gomoku.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UtilityTest {
+
+    @ParameterizedTest
+    @CsvSource({"-10, true", "-11, false", "-2,true", "-1, false", "0, true", "1, false", "2, true", "10, true", "11, false"})
+    void isEvenNumber(int numberToTest, boolean isEven) {
+        assertEquals(isEven, Utility.isEvenNumber(numberToTest));
+    }
+}
