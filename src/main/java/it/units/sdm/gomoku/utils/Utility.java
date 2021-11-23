7f97138105cd560a4f92cffdf4273ce1d405ccc3
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-11-23 02:13:34 +0100

    Update class Utility

--- src/main/java/it/units/sdm/gomoku/utils/Utility.java
+++ src/main/java/it/units/sdm/gomoku/utils/Utility.java
@@ -11,27 +11,8 @@ public class Utility {
         return n % 2 == 0;
     }
 
-    public static Logger getLoggerOfClass(@NotNull final Class<?> targetClass) {    // TODO: test?
+    public static Logger getLoggerOfClass(@NotNull final Class<?> targetClass) {
         return Logger.getLogger(Objects.requireNonNull(targetClass).getCanonicalName());
     }
 
-    //    private static int threadCount = 0;
-    public static Thread runOnSeparateThread(@NotNull final Runnable runnable) {    //TODO: test?
-        var t = new Thread(runnable);
-//        var name = t.getName();
-//        new Thread(() -> {
-//            System.out.println(name + " starting... (" + threadCount + " threads were already running)");
-        t.start();
-//            threadCount++;
-//            try {
-//                t.join();
-//                threadCount--;
-//                System.out.println(name + " ended (" + threadCount + " threads are still running)");
-//            } catch (InterruptedException e) {
-//                System.out.println(name + " interrupted wtf (" + threadCount + " threads are running)");
-//            }
-//
-//        }).start();
-        return t;
-    }
 }
